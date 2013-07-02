6eb3e1e26cbc7a86f08f9dea8efea1fe32e624e4
Lambert <lambert@yahoo-inc.com>
2013-07-01 17:27:15 -0700

    updates

--- src/com/yahoo/rssreader/RSSReaderDetails.java
+++ src/com/yahoo/rssreader/RSSReaderDetails.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,7 +21,16 @@ public class RSSReaderDetails extends Activity {
 		tvURL = (TextView)this.findViewById(R.id.DataURL);
 		
 	}
+	
+	// Waiting for info from Sameer to get data to put into Details Page
+	
+	 // Push results to Text View
+	 protected void showResult(TextView tvTitle) 
+	 {
+		tvTitle.setText("Hello Title");
+	 }
 
+	// Not used: In case there's a need to inflate the menu
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -31,6 +38,7 @@ public class RSSReaderDetails extends Activity {
 		return true;
 	}
 	
+	// Not used: In case we want to add refresh feature on details page
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 	    // Handle item selection
@@ -44,6 +52,7 @@ public class RSSReaderDetails extends Activity {
 	    }
 	}
 	
+	// Not used: In case we want to add refresh feature on the details page
 	private void refreshFragment() {
 		//TEMP
 		Toast.makeText(this, "Clicked on Refresh", Toast.LENGTH_SHORT).show();
