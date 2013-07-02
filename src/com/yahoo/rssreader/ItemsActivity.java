c8e81bf26ca63dc78969cc0a9bbbc05177055b72
Sameer Shah <ssameer@yahoo-inc.com>
2013-07-01 19:10:59 -0700

    FeedItems
    ItemsActivity

--- src/com/yahoo/rssreader/ItemsActivity.java
+++ src/com/yahoo/rssreader/ItemsActivity.java
@@ -1,15 +1,27 @@
 package com.yahoo.rssreader;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.yahoo.rssreader.models.Feed;
+import com.yahoo.rssreader.models.Item;
 
 public class ItemsActivity extends FragmentActivity {
+	
+	ArrayAdapter<Item> aItems;
+	ListView lvItems;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,19 +32,43 @@ public class ItemsActivity extends FragmentActivity {
 		if(bundle != null){
 			if(bundle.containsKey("feed")){
 				Feed feed = (Feed)bundle.get("feed");
-				Log.d("DEBUG", "Feed{" + feed + "}");
-				Toast.makeText(this, "Feed{" + feed + "}", Toast.LENGTH_SHORT).show();
-				
-				Intent i = new Intent(Intent.ACTION_VIEW);
-				i.setData(Uri.parse(feed.getItem(0).getLink()));
-				startActivity(i);
-				
+//				Log.d("DEBUG", "Feed{" + feed + "}");
+//				Toast.makeText(this, "Feed{" + feed + "}", Toast.LENGTH_SHORT).show();
+				List<Item> items = feed.items();
+				aItems = new ItemsArrayAdapter(this, items);
+		        
+				lvItems = (ListView) findViewById(R.id.listView1);
+		        lvItems.setAdapter(aItems);
+		        lvItems.setOnItemClickListener(new OnItemClickListener() {
+
+					@Override
+					public void onItemClick(AdapterView<?> parent, View view, int position,
+							long id) {
+						
+						Item item = aItems.getItem(position);
+						//Toast.makeText(SubscriptionsFragment.this.getActivity(),"CLICKING ON ITEM " + feed, Toast.LENGTH_SHORT).show();
+						/*
+						 * Use this for Lamberts code
+						 */
+//						Intent intent = new Intent(getApplicationContext(), RSSReaderDetails.class);
+//						Bundle bundle = new Bundle();
+//						bundle.putSerializable("item", item);
+//						intent.putExtras(bundle);
+//						startActivity(intent);
+						
+						/*
+						 * Use this to directly open in WebBrowser
+						 */
+						//Opening a web browser:
+						Intent webPageIntent = new Intent(Intent.ACTION_VIEW);
+						webPageIntent.setData(Uri.parse(item.getLink()));
+						startActivity(webPageIntent);
+					}
+				});
 			} else {
 				Toast.makeText(this, "Feed not found in bundle", Toast.LENGTH_SHORT).show();
 			}
 		
-			
-			
 		} else {
 			Toast.makeText(this, "Bundle is null", Toast.LENGTH_SHORT).show();
 		}
---
+++ src/com/yahoo/rssreader/ItemsArrayAdapter.java
@@ -0,0 +1,35 @@
+package com.yahoo.rssreader;
+
+import java.util.List;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.yahoo.rssreader.models.Item;
+
+public class ItemsArrayAdapter extends ArrayAdapter<Item> {
+
+	public ItemsArrayAdapter(Context context, List<Item> items) {
+		super(context, R.layout.activity_items, items);
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		LayoutInflater vi = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		LinearLayout v = (LinearLayout) vi.inflate(R.layout.feed_item, null);
+		Log.d("test","test");
+		Item item = this.getItem(position);
+		TextView tvItemTitle = (TextView) v.findViewById(R.id.tvItemTitle);
+		//TextView tvItemDesc = (TextView) v.findViewById(R.id.tvItemDescription);
+		tvItemTitle.setText(item.getTitle());
+		//tvItemDesc.setText(item.getDescription());
+		return v;				
+	}
+
+}
